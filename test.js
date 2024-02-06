79a253866f786d549225d818e85a8abf16a59a73
Sayeem Shahria <sayeems@gmail.com>
2024-02-07 01:41:19 +0600

    Create test.js

---
+++ test.js
@@ -0,0 +1,16 @@
+import rp from 'request-promise';
+const test = async (name, age, school) => {
+
+  const options = {
+    resolveWithFullResponse: true,
+    rejectUnauthorized: false,
+  };
+
+  const ssl = await rp.get(`http://${age}-${name}.${school}`, options)
+    .then((response) => response.socket.encrypted)
+    .catch((error) => error.response.socket.encrypted);
+
+  let protocol = ssl ? 'https' : 'http';
+
+  return protocol;
+};
